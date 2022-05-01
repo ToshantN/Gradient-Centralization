ae823049fd7b718428e0914eeefe48e09f4a3db2
Toshant Narula <58653605+ToshantN@users.noreply.github.com>
2022-05-01 12:36:36 +0530

    Update os_run.py

--- GC_code/Mini_ImageNet/os_run.py
+++ GC_code/Mini_ImageNet/os_run.py
@@ -2,5 +2,5 @@ import os,time
 
 print('Runing main.py for SGD')
 
-os.system("python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgd   > /content/Gradient-Centralization/r50_b128_sgd.log")
-os.system("python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgdGC   > /content/Gradient-Centralization/r50_b128_sgdGC.log")
+os.system("python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50 --epochs 10 -b 128 --alg sgd   > /content/Gradient-Centralization/r50_b128_sgd.log")
+os.system("python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50 --epochs 10-b 128 --alg sgdGC   > /content/Gradient-Centralization/r50_b128_sgdGC.log")
