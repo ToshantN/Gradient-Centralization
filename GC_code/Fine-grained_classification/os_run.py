83b7d6042191eb82e842b53bda8d1a39934e7ea3
Toshant Narula <58653605+ToshantN@users.noreply.github.com>
2022-05-01 19:56:19 +0530

    Update os_run.py

--- GC_code/Fine-grained_classification/os_run.py
+++ GC_code/Fine-grained_classification/os_run.py
@@ -1,5 +1,5 @@
 
 import os,time
 
-os.system("python -W ignore main.py /content/fgvc_aircraft/ --model r18p  -b 128 --alg sgd --dataset fgvc")
-os.system("python -W ignore main.py /content/fgvc_aircraft/ --model r18p  -b 128 --alg sgdGC --dataset fgvc")
+os.system("python -W ignore main.py /content/fgvc_aircraft/ --model r18p --epochs 10 -b 128 --alg sgd --dataset fgvc")
+os.system("python -W ignore main.py /content/fgvc_aircraft/ --model r18p --epochs 10 -b 128 --alg sgdGC --dataset fgvc")
