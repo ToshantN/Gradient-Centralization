0b16a9bd10494f2b363efa46e9842bbb97a4ecfa
Toshant Narula <58653605+ToshantN@users.noreply.github.com>
2022-05-01 19:13:43 +0530

    Update os_run.py

--- GC_code/CIFAR100/os_run.py
+++ GC_code/CIFAR100/os_run.py
@@ -4,7 +4,7 @@ import os,time
 #cifar100 sgd & sgdGCC
 
 print("Running main.py with SGD")
-os.system("python  main.py --lr 0.1 --wd 0.0005 --alg sgd --epochs 10  --model r18 > /content/r18_sgd.log")
+os.system("python  main.py --lr 0.1 --wd 0.0005 --alg sgd --epochs 10  --model r18")
 
 print("Running main.py with SGDGC")
-os.system("python  main.py --lr 0.1 --wd 0.0005 --alg sgdGC --epochs 10  --model r18 > /content/r18_sgdGC.log")
+os.system("python  main.py --lr 0.1 --wd 0.0005 --alg sgdGC --epochs 10  --model r18")
